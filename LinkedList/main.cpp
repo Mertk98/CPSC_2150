76e5231358853e6875065d220f7322bd02331fd5
Mert Kilic <Mert@Karlas-MacBook-Air.local>
2022-05-18 12:43:17 -0700

    Linked List bug fixes and new test cases

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -9,30 +9,32 @@ int& change(int a[], int i)
 int main()
 {
     LList<int> A;
-    for(int i = 0; i < 40; i++)
+    for(int i = 0; i < 20; i++)
     {
         A.insert((int)(rand() % 100));
     }
 
-    // A.twice();
-    // std::cout << A << std::endl;
-    // std::cout << A.length() << std::endl;
-
-    // LList B;
-    // for(int i = 0; i < 40; i++)
-    // {
-    //     B.insert((int)(rand() % 100));
-    // }
-    // std::cout << B << std::endl;
-    // std::cout << B.length() << std::endl;
-
-    // B = A;
-    // std::cout << B << std::endl;
-    // std::cout << B.length() << std::endl;
     std::cout << A << std::endl;
-    std::cout << A.halve() << std::endl;
+    A.twice();
     std::cout << A << std::endl;
+    std::cout << A.length() << std::endl;
 
+    LList<int> B;
+    for(int i = 0; i < 20; i++)
+    {
+        B.insert((int)(rand() % 100));
+    }
+    std::cout << B << std::endl;
+    std::cout << B.length() << std::endl;
 
+    B = A;
+    std::cout << B << std::endl;
+    std::cout << B.length() << std::endl;
+    std::cout << A << std::endl;
+    LList<int> C;
+    C = A.halve();
+    std::cout << C << std::endl;
+    std::cout << C.length() << std::endl;
+    std::cout << A << std::endl;
     return 0;
 }
