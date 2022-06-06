f712c32ecd75ed3b941ebf836fb4007a1ce452d2
Mert Kilic <Mert@Karlas-MacBook-Air.local>
2022-06-06 11:30:16 -0700

    Updated Lab4.cpp

--- Lab4/lab4.cpp
+++ Lab4/lab4.cpp
@@ -2,10 +2,43 @@
 #include "StackEmpty.h"
 #include "Stack.h"
 
-// std::string in2prefix(const std::string& in)
-// {
+std::string in2prefix(const std::string& in, Stack<char> ops, Stack<std::string> opands)
+{
+	std::string str = "";
+	if(in.length() == 0)
+	{
+		return "";
+	}
+
+	else if(in[0] == '(' || in[0] == '+' || in[0] == '-'
+	|| in[0] == '*' || in[0] == '/' || in[0] == '%')
+	{
+		ops.push(in[0]);
+	}
 	
-// }
+	else if(in[0] == ')')
+	{
+		while(!opands.isEmpty())
+		{
+			str = opands.peek() + str;
+			opands.pop();
+		}
+		str = ops.peek() + str;
+		ops.pop();
+		ops.pop();
+	}
+
+	else
+	{
+		if(in[0] != ' ')
+		{
+			std::string tmp(1,in[0]);
+			opands.push(tmp);
+		}
+	}
+	
+	return str + in2prefix(in.substr(1, in.length()), ops, opands);
+}
 
 //std::string pre2infix(std::string pre)
 //{
@@ -20,6 +53,11 @@
 
 int main()
 {
-	std::cout << "test" << std::endl;
+	Stack<char> ops;
+	Stack<std::string> opands;
+	std::string exp = "(( x%d )+(Y-c))";
+
+	std::cout << in2prefix(exp, ops, opands) << std::endl;
+
 	return 0;
 }
