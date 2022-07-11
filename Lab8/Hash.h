00ab6e2bc63bb1edb5349e1e13f059ed4f6d5307
Mert Kilic <Mert@Karlas-MacBook-Air.local>
2022-07-11 09:46:03 -0700

    Updated the class Hash

--- Lab8/Hash.h
+++ Lab8/Hash.h
@@ -1,15 +1,19 @@
 #ifndef _HASH_H_
 #define _HASH_H_
-
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <fstream>
 
 class Hash
 {
+private:
     const int SIZE = (int)std::pow(2,16);
     std::vector<int> hashTable = std::vector<int>(SIZE);
+    int hashFunction(std::string word);
+public:
+    Hash(std::string fileName);
 
-}
+};
 
 #endif // _HASH_H_
